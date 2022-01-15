eb274b70b8007a67231d78c0e323b0e51eeb7994
Akash Gautam <68077850+MrGautam@users.noreply.github.com>
2022-01-15 17:13:47 +0530

    Add files via upload

---
+++ LMS/AdminRegistrationRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS_App.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace LMS_App
+{
+    public class AdminRegistrationRepository : IAdminRegistrationRepository<AdminRegistration>
+    {
+        SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString);
+        SqlCommand sqlCommand = new SqlCommand();
+        SqlDataReader sqlDataReader;
+        public bool DeleteAdminRegistration(AdminRegistration AdminRegistration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AdminRegistration> GetAllAdminRegistrationRepositorys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AdminRegistration GetAdminRegistration(int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveAdminRegistration(AdminRegistration AdminRegistration)
+        {
+            try
+            {
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText= "insert into AdminRegistration values(" + AdminRegistration.email + ",'" + AdminRegistration.first_name + "','" + AdminRegistration.last_name + "','" + AdminRegistration.AdminPassword + "')";
+                if (sqlConnection.State ==System.Data.ConnectionState.Closed)
+                {
+                    sqlConnection.Open();
+                }
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateAdminRegistration(AdminRegistration AdminRegistrationRepository)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
